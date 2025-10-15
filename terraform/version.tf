5c286b37c4969276cda8dd8446010e76bf2a0149
balachandar jayaraman <balachandare@gmail.com>
2025-10-15 22:51:24 +0530

    updated  version

--- terraform/version.tf
+++ terraform/version.tf
@@ -1,16 +1,9 @@
 terraform {
   required_version = ">= 1.0"
 
-  required_version = ">= 1.0"
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "~> 2.23"
       version = "~> 5.0"
     }
     kubernetes = {

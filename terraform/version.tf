b4f00508aa8c1cf90d6af011759a1c8970a9685a
balachandar jayaraman <balachandare@gmail.com>
2025-10-15 23:06:58 +0530

    helm updation

--- terraform/version.tf
+++ terraform/version.tf
@@ -17,7 +17,6 @@ terraform {
   }
 }
 
-
 provider "aws" {
   region = var.aws_region
 }
@@ -33,15 +32,4 @@ provider "kubernetes" {
   }
 }
 
-provider "helm" {
-  kubernetes {
-    host                   = module.eks.cluster_endpoint
-    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-
-    exec {
-      api_version = "client.authentication.k8s.io/v1beta1"
-      command     = "aws"
-      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
-    }
-  }
-}
+provider "helm" {}

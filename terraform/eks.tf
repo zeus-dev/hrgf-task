8f2ef767108eb2dd0195f104cc8f04adaa9d34c3
balachandar jayaraman <balachandare@gmail.com>
2025-10-16 10:43:20 +0530

    update EKS module version to 20.31.0 and add Terraform lock files

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -1,6 +1,6 @@
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "~> 20.0.0"
+  version = "~> 20.31.0"
 
   cluster_name    = var.cluster_name
   cluster_version = var.cluster_version
